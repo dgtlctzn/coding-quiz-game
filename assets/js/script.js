f7f5818d07c49473d5d9e26735be94c7417824d3
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-09-10 15:01:52 -0400

    local storage working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,20 @@
 $(document).ready(function () {
+  /**
+   * DOM VARIABLES
+   */
   var questionDiv = $("#question");
-  //   console.log(questions);
-  var currentQuestionIndex = 0;
+  var correctIndicatorEl = $("#correct-indicator");
 
+  /**
+   * VARIABLES
+   */
+  var currentQuestionIndex = 0;
+  var timeRemaining = 90;
+  var timeInterval;
+
+  /**
+   * FUNCTION DEFINITIONS
+   */
   function displayQuestion() {
     // Show the first question.
     questionDiv.empty();
@@ -17,6 +29,7 @@ $(document).ready(function () {
       var buttonEl = $("<button>");
       buttonEl.addClass("btn btn-secondary answer");
       buttonEl.text(questions[currentQuestionIndex].choices[i]);
+      buttonEl.attr("data-choice", questions[currentQuestionIndex].choices[i]);
       liEl.append(buttonEl);
       ulEl.append(liEl);
     }
@@ -24,24 +37,65 @@ $(document).ready(function () {
     questionDiv.append(ulEl);
   }
 
-  $("#start-game").on("click", function () {
-    // Hide the welcome screen.
-    $("#welcome-screen").attr("style", "display: none");
-    // Start the timer
-    displayQuestion();
+  function setAndClearIndicator(text) {
+    correctIndicatorEl.text(text);
+    setTimeout(function () {
+      correctIndicatorEl.text("");
+    }, 1000);
+  }
 
-    // alert("Start the game!");
+  /**
+   * FUNCTION CALLS
+   */
+
+  /**
+   * EVENT LISTENERS
+   */
+  $("#start-game").on("click", function () {
+    $("#welcome-screen").attr("style", "display: none");
+    timeInterval = setInterval(function () {
+      $("#time-remaining").text(timeRemaining);
+      timeRemaining--;
+    }, 1000);
+
+    displayQuestion();
   });
 
   $("#question").on("click", ".answer", function () {
-    console.log($(this));
-    console.log("Clicked an element with class answer.");
+    if (
+      $(this).attr("data-choice") === questions[currentQuestionIndex].answer
+    ) {
+      setAndClearIndicator("Correct!");
+    } else {
+      setAndClearIndicator("Incorrect!");
+      timeRemaining = timeRemaining - 10;
+    }
     if (currentQuestionIndex < questions.length - 1) {
       currentQuestionIndex++;
       displayQuestion();
     } else {
+      clearInterval(timeInterval);
       questionDiv.attr("style", "display: none");
       $("#save-screen").attr("style", "display: block");
     }
+  });
+
+  $("#save-game").on("click", function () {
+    var highScores = JSON.parse(localStorage.getItem("highscores"));
+
+    if (!highScores) {
+      highScores = [];
+    }
+
+    var userInitials = $("#user-initials").val();
+    console.log(userInitials);
+    // Array of objects to hold the information
+
+    var currentHighScore = {
+      user: userInitials,
+      score: timeRemaining,
+    };
+    highScores.push(currentHighScore);
+    localStorage.setItem("highscores", JSON.stringify(highScores));
   });
 });
